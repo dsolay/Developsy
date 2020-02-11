2748ab830b456cfa4e9bade7f51c8bbfd9a957fc
Roman Gonzalez <solay2516@gmail.com>
2020-02-11 08:00:31 -0600

    fix timezone

--- images/base/build.sh
+++ images/base/build.sh
@@ -115,7 +115,9 @@ apt-install swaks
 echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bashrc
 
 # Set timezone
+apt-install tzdata
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
 
 # Alias for tree view of commit history.
 git config --global alias.tree "log --all --graph --decorate=short --color --format=format:'%C(bold blue)%h%C(reset) %C(auto)%d%C(reset)\n         %C(blink yellow)[%cr]%C(reset)  %x09%C(white)%an: %s %C(reset)'"
