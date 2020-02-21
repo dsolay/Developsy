2e1b6ef99c65f951966a1c5d849c0aafc633347d
Roman Gonzalez <solay2516@gmail.com>
2020-02-21 15:15:39 -0600

    vnc server

---
+++ images/vnc/vncserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# entrypoint.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server, pulling the code from git and then running the test.
+export DISPLAY=:20
+Xvfb :20 -screen 0 1024x768x16 &
+x11vnc -rfbauth ~/.passwordvnc -display :20 -N -forever &
+exec openbox-session
+wait
